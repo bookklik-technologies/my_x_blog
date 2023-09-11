02279cc40f9775e2fc85f4b11f861b66a83b014d
A.Hakim Noor <a.hakim.solution@gmail.com>
2023-09-11 22:09:16 +0800

    add keywords to post migration

--- database/migrations/2023_08_25_112343_create_posts_table.php
+++ database/migrations/2023_08_25_112343_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
+            $table->string('keywords')->nullable();
             $table->text('body');
             $table->string('status')->default('draft')->nullable();
             $table->unsignedBigInteger('author_id');
